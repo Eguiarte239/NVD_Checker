fdc28471b9ce81e50d8c378976fadfa0268d37cb
Abraham Eguiarte <abraham.lopez5085@alumnos.udg.mx>
2022-12-03 19:39:18 -0600

    Code improved

--- api.py
+++ api.py
@@ -1,14 +1,20 @@
 import requests, json
 from datetime import date
 from datetime import timedelta
+from urllib.error import HTTPError
 import pandas as pd
-today = date.today()
-lastWeek = today - timedelta(days=7)
-url = "https://services.nvd.nist.gov/rest/json/cves/2.0/?pubStartDate="+str(lastWeek)+"T00:00:00.000&pubEndDate="+str(today)+"T00:00:00.000"
-request = requests.get(url)
-response = json.loads(request.text)
-descriptionsList = list()
-for item in response['vulnerabilities']:
-    descriptionsList.append(item['cve']['descriptions'][0]['value'])
-df = pd.DataFrame(descriptionsList, columns=['q_data'])
-df.to_excel("cveDescriptions.xlsx")
+try:
+    today = date.today()
+    lastWeek = today - timedelta(days=7)
+    url = "https://services.nvd.nist.gov/rest/json/cves/2.0/?pubStartDate="+str(lastWeek)+"T00:00:00.000&pubEndDate="+str(today)+"T00:00:00.000"
+    req = requests.get(url)
+    if req.status_code != 200:
+        raise ValueError
+    response = json.loads(req.text)
+    descriptionsList = list()
+    for item in response['vulnerabilities']:
+        descriptionsList.append(item['cve']['descriptions'][0]['value'])
+    df = pd.DataFrame(descriptionsList, columns=['q_data'])
+    df.to_excel("cveDescriptions.xlsx")
+except ValueError as e:
+    print(e)
